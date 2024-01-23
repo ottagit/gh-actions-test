f55fc2cf01ce63f766a4b86fb5c7052834e08f3d
ottagit <ottaosawa@gmail.com>
2024-01-23 18:37:09 +0300

    initial commit

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "ami" {
+   type        = string
+   description = "Ubuntu AMI ID in N. Virginia Region"
+   default     = "ami-0c7217cdde317cfec"
+}
+
+variable "instance_type" {
+   type        = string
+   description = "Instance type"
+   default     = "t2.micro"
+}
+
+variable "name_tag" {
+   type        = string
+   description = "Name of the EC2 instance"
+   default     = "Test EC2 Instance"
+}
