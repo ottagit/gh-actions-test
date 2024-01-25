f71249d415b07d04d2a8943efef5fbb67f3122a4
ottagit <ottaosawa@gmail.com>
2024-01-25 09:12:32 +0300

    add GitHub Actions workflow to run Terraform

---
+++ provider.tf
@@ -0,0 +1,17 @@
+terraform {
+ required_providers {
+   aws = {
+     source  = "hashicorp/aws"
+     version = "~> 4.18.0"
+   }
+ }
+
+ backend "s3" {
+     bucket = "batoto-bitange"
+     key = "global/s3/terraformjenkins.tfstate"
+     region = "us-east-1"
+ 
+     dynamodb_table = "terraone-locks"
+     encrypt = true
+   }
+}
