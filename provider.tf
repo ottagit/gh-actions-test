e8d3e22fb44d8d9b1beafdee09637faae60b23a9
ottagit <ottaosawa@gmail.com>
2024-01-25 09:39:48 +0300

    update path to TF state key

--- provider.tf
+++ provider.tf
@@ -8,7 +8,7 @@ terraform {
 
  backend "s3" {
      bucket = "batoto-bitange"
-     key = "global/s3/terraformjenkins.tfstate"
+     key = "global/iam/github-actions/terraformjenkins.tfstate"
      region = "us-east-1"
  
      dynamodb_table = "terraone-locks"
